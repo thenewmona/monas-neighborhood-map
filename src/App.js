85b51ea413b24cd908f80dc53624d1f4948db501
Ramona Saintandre <thenewmona@gmail.com>
2018-12-23 10:16:33 -0500

    commented out mapcontent for right now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import Mapcontent from './components/Mapcontent'
+// import Mapcontent from './components/Mapcontent'
 
 
 
--- src/components/Mapcontent/Mapcontent.js
+++ src/components/Mapcontent/Mapcontent.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Museummap from './Component/Museummap'
-import Museumlist from './Component/Museumlist'
+// import React from 'react';
+// import Museummap from './Component/Museummap'
+// import Museumlist from './Component/Museumlist'
 
 
-class Mapcontent extends React.Component {
-    render() {
-return (
-    <Museummap />
-)
+// class Mapcontent extends React.Component {
+//     render() {
+// return (
+//     <Museummap />
+// )
 
-    }
-}
+//     }
+// }
 
-export default Mapcontent;
+// export default Mapcontent;
