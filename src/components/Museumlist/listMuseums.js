95d78fe4373080a435f8755e22bf46467b4349db
Ramona Saintandre <thenewmona@gmail.com>
2019-01-19 17:53:32 -0500

    created listMuseum component

---
+++ src/components/Museumlist/listMuseums.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class listMuseums extends Component {
+  render() {
+    return (<li className='listMuseumsIndex'>
+    Hello Grand Rapids
+    </li>
+      
+    )
+  }
+}
+
